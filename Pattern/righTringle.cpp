4ec5de2584209caf502906c9c731e0643e65cfb7
Nitish Pandit <nitishpandit95087546@gmail.com>
2024-10-02 14:03:49 +0530

    Add inverted tringle printing

--- Pattern/righTringle.cpp
+++ Pattern/righTringle.cpp
@@ -5,8 +5,17 @@ int main() {
     int n;
     cout << "Enter number of rows: ";
     cin >> n;
-
+    
+    // right tringle 
     for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // inverted right tringle
+     for (int i = n; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
@@ -15,3 +24,6 @@ int main() {
 
     return 0;
 }
+
+
+
